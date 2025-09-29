9c1dfa4aedf3382c61593ed9a41d7772f49de3a3
San1tater <98987733+San1tater1122@users.noreply.github.com>
2025-09-29 19:52:41 -0400

    Delete src/model/Employee.java

--- src/model/Employee.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-// Responsible by San1tater
-
-public class Employee extends User{
-    public Employee(short userID, String email, String password){
-        super(userID, email, password);
-    }
-}
