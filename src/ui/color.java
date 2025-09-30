7f31d64cd31e117201e9f4097c4fdb52925a2410
V <90130404+MicroNeutron@users.noreply.github.com>
2025-09-30 11:39:23 -0400

    创建颜色created color.java
    神说要有光

--- src/ui/color.java
+++ src/ui/color.java
@@ -1,1 +1,17 @@
+package ui;
+//created by MicroNeutron
 
+class Colours {
+    //font colour start
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String BLUE = "\u001B[38;5;33m";
+    //background colour start
+    public static final String BG_BLACK = "\u001B[40m";
+    public static final String BG_GREEN = "\u001B[42m";
+    public static final String BG_BLUE = "\u001B[44m";
+    public static final String BG_PURPLE = "\u001B[45m";
+    public static final String BG_CYAN = "\u001B[46m";
+    public static final String BG_WHITE = "\u001B[47m";
+} 
