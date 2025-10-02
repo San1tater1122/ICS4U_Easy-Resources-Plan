687aed5adad3534dce373065bf12be381b3c0a48
San1tater <98987733+San1tater1122@users.noreply.github.com>
2025-10-02 12:57:45 -0400

    优化

--- src/main/Primenu.java
+++ src/main/Primenu.java
@@ -9,49 +9,49 @@ import ui.Colours;
 public class Primenu {
     public static void primenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println(Colours.BLUE + "ERP Menu");
-        System.out.println(Colours.RESET + "1: Products Information");
-        System.out.println(Colours.RESET + "2: Purchasing Management");
-        System.out.println(Colours.RESET + "3: Sales Management");
-        System.out.println(Colours.RESET + "4: Storage Management");
-        System.out.println(Colours.RESET + "5: System Parameter");
-        System.out.println(Colours.RESET + "6: Consultant & Statement");
-        System.out.println(Colours.RESET + "7: User Permission");
-        System.out.println(Colours.RESET + "8: Log Out");
-        System.out.println(Colours.BLUE + "Choose Function to Use:");
+        System.out.println(Colours.BLUE + "ERP Menu" + Colours.RESET);
+        System.out.println("1: Products Information");
+        System.out.println("2: Purchasing Management");
+        System.out.println("3: Sales Management");
+        System.out.println("4: Storage Management");
+        System.out.println("5: System Parameter");
+        System.out.println("6: Consultant & Statement");
+        System.out.println("7: User Permission");
+        System.out.println("8: Log Out");
+        System.out.println("Choose Function to Use:");
 
         String choice = sc.nextLine();
         switch (choice) {
             case "1":
-                System.out.print(Colours.GREEN + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.GREEN + "前面的区域，以后再来探索吧" + Colours.RESET);
                 break;
 
             case "2":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.RESET + "前面的区域，以后再来探索吧");
                 break;
 
             case "3":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.RESET + "前面的区域，以后再来探索吧");
                 break;
                     
             case "4":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.RESET + "前面的区域，以后再来探索吧");
                 break;
 
             case "5":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.RESET + "前面的区域，以后再来探索吧");
                 break;
                     
             case "6":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.RESET + "前面的区域，以后再来探索吧");
                 break;
 
             case "7":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                System.out.println(Colours.RESET + "前面的区域，以后再来探索吧");
                 break;
 
             case "8":
-                System.out.print(Colours.RESET + "前面的区域，以后再来探索吧");
+                LogOut.logout();
                 break;
 
             default:
